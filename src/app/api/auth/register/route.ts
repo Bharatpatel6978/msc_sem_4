0d136bebb987661ecf6bed60bdab1ada498ee22c
bharatpatel6978~ <bharatmungalpara664@gmail.com>
2025-12-02 19:53:46 +0530

    register page done and login page design done

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -3,10 +3,12 @@ import User from "@/models/user.model";
 import bcrypt from "bcryptjs";
 import { NextRequest , NextResponse } from "next/server";
 
+
 export async function POST(req:NextRequest) {
     try{
         await connectDb()
         const {name, email ,password} = await req.json()
+        
         const existUser = await User.findOne({email})
         if(existUser){
             return NextResponse.json(
---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import {motion} from "motion/react"
+import { EyeIcon, EyeOff, Key, Leaf, Loader2, Lock, LogIn, Mail, User } from 'lucide-react'
+import { tr } from 'motion/react-client'
+import axios from 'axios'
+import { log } from 'console'
+import { useRouter } from 'next/navigation'
+
+
+const RegisterForm = () => {
+    const [name,setName] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const [showPassword,setShowPassword] = useState(false)
+    const [loading,setLoading] = useState(false)
+    const router = useRouter()
+    const handleRegister  = async (e:React.FormEvent)=>{
+        e.preventDefault()
+        setLoading(true)
+        try{
+            const result = await axios.post("/api/auth/register",{
+                name,email,password
+            })
+            console.log(result.data);
+            setLoading(false)
+            
+        }catch(error){
+            console.log(error);
+            setLoading(false)
+            
+        }
+    }
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen px-6 py-10 bg-white relative'>
+        <motion.h1 initial={{
+            y:-10,
+            opacity:0
+        }}
+        animate={{
+            y:0,
+            opacity:1
+        }}
+        transition={{
+            duration:0.6
+        }} className='text-4xl font-extrabold text-green-700 mb-2'>Create Account</motion.h1>
+        <p className='text-gray-600 mb-8 flex items-center'>Join SK MART today<Leaf className='w-5 h-5 text-green-600 '/></p>
+
+        <motion.form onSubmit={handleRegister}  initial={{
+            opacity:0
+        }}
+        animate={{
+            opacity:1
+        }}
+        transition={{
+            duration:0.6
+        }}  className='flex flex-col gap-5 w-full max-w-sm'>
+
+            <div className="relative">
+                <User className='absolute left-3 top-3.5 w-5 h-5 text-gray-400'/>
+                <input type="text" placeholder='Your Name'  className='w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:ring-green-500 focus:outline-none'
+                onChange={(e)=>setName(e.target.value)}
+                value={name}
+                />
+            </div>
+            <div className="relative">
+                <Mail className='absolute left-3 top-3.5 w-5 h-5 text-gray-400'/>
+                <input type="text" placeholder='Your Email'  className='w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:ring-green-500 focus:outline-none'
+                onChange={(e)=>setEmail(e.target.value)}
+                value={email}
+                />
+            </div>
+            <div className="relative">
+                < Lock className='absolute left-3 top-3.5 w-5 h-5 text-gray-400'/>
+                <input type={showPassword?"text":"password"} placeholder='Your Password'  className='w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:ring-green-500 focus:outline-none'
+                onChange={(e)=>setPassword(e.target.value)}
+                value={password}
+                />
+                {
+                    showPassword?<EyeOff className='absolute right-3 top-3.5 w-5 h-5 text-gray-500 cursor-pointer' onClick={()=> setShowPassword(false)}/>:<EyeIcon className='absolute right-3 top-3.5 w-5 h-5 text-gray-500 cursor-pointer' onClick={()=>setShowPassword(true)}/>
+                }
+            </div>
+
+            {
+                (()=>{
+                    const formValidation = name!=="" && email!=="" && password!==""
+                    return <button disabled={!formValidation || loading} className={`w-full font-semibold py-3 rounded-xl transition-all
+                    duration-200 shadow-md inline-flex items-center justify-center gap-2 ${
+                        formValidation 
+                        ? "bg-green-600 hover:bg-green-700 text-white cursor-pointer"
+                        : "bg-gray-300 text-gray-500 cursor-not-allowed "
+                    }`}>
+
+                        {loading?<Loader2 className='w-5 h-5 animate-spin'/>: "Register"}
+                    </button>
+                })()
+            }
+
+
+        </motion.form>
+
+        <p className="cursor-pointer text-gray-600 mt-6 text-sm flex items-center gap-1" onClick={()=> router.push("/login")}>
+            Already have an account ? <LogIn className='w-4 h-4 text-green-600'/><span className='text-blue-500'>sign in</span>
+        </p>
+    </div>
+  )
+}
+
+export default RegisterForm
