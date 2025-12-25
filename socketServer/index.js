04120961d4115c796665bd0951c2355299d0eac0
bharatpatel6978~ <bharatmungalpara664@gmail.com>
2025-12-25 12:55:21 +0530

    location geting done

--- socketServer/index.js
+++ socketServer/index.js
@@ -1,34 +1,46 @@
-import express  from "express"
-import dotenv from "dotenv"
-import http from 'http'
-import { Server } from "socket.io"
-import axios from "axios"
-import { Socket } from "dgram"
-dotenv.config()
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+import axios from "axios";
+import { Socket } from "dgram";
+import { type } from "os";
+dotenv.config();
+const app = express();
 
-const server  = http.createServer(app)
-const port = process.env.PORT || 5000
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
 
-const io = new Server(server,{
-    cors:{
-        origin:process.env.NEXT_BASE_URL
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: process.env.NEXT_BASE_URL,
+  },
+});
 
-io.on("connection",(socket)=>{
-    console.log("user connected",socket.id);
+io.on("connection", (socket) => {
+  socket.on("identity", async (userId) => {
+    await axios.post(`${process.env.NEXT_BASE_URL}/api/socket/connect`, {
+      userId,
+      socketId: socket.id,
+    });
+  });
 
-    socket.on("identity",async (userId)=>{
-        console.log(userId);
-        await axios.post(`${process.env.NEXT_BASE_URL}/api/socket/connect`,{userId,socketId:socket.id})
-    })
-    socket.on("disconnect",()=>{
-        console.log("user disconnected",socket.id);
-    })
-    
-})
-server.listen(port,(
-    console.log("server started at" ,port)
-    
-))
+  socket.on("update-location", async ({ userId, latitude, longitude }) => {
+      
+      const location = {
+          type: "Point",
+          coordinates: [longitude, latitude],
+        };
+        
+    await axios.post(
+      `${process.env.NEXT_BASE_URL}/api/socket/update-location`,
+      { userId, location }
+    );
+
+   
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+server.listen(port, console.log("server started at", port));
